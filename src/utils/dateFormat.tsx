a5eee5f75c888599212470af80085b8a7afbd722
ngoclinh1103 <ngoclinhnn.ptit@gmail.com>
2025-11-29 12:32:02 +0700

    Delete src/utils directory

--- src/utils/dateFormat.tsx
+++
@@ -1,6 +0,0 @@
-import moment from 'moment'
-
-export const dateFormat = (date: string | Date | undefined): string => {
-    if (!date) return ''
-    return moment(date).format('DD/MM/YYYY')
-}
--- src/utils/formatAmount.ts
+++
@@ -1,6 +0,0 @@
-export const formatAmount = (value: number): string => {
-  if (value >= 1_000_000_000) return (value / 1_000_000_000).toFixed(1) + "B VNĐ";
-  if (value >= 1_000_000) return (value / 1_000_000).toFixed(1) + "M VNĐ";
-  if (value >= 1_000) return (value / 1_000).toFixed(0) + "K VNĐ";
-  return value.toLocaleString("vi-VN") + " VNĐ";
-};
